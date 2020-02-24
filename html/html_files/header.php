87aa9c29a8bcc21138b6f0fef09489035d90c25b
silviu <silviuilas@gmail.com>
2020-02-24 17:06:57 +0200

    Silviu NavBarFix

--- html/html_files/header.php
+++ html/html_files/header.php
@@ -4,16 +4,18 @@
 <html lang="en">
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/search.css">
     <link rel="icon" href="https://cdn4.iconfinder.com/data/icons/money-and-finance/512/find-512.png">
     <title>COMP IT</title>
+    <nav class="navbar" >
+        <a href="/js/">Logo</a>
+        <a href="/js/">CompIt</a>
+        <input type="text" placeholder=" Search..">
+        <a href="/js/">Favorite</a>
+        <a href="/js/">Login</a>
+    </nav>
 </head>
 
 
-<nav class="navbar" >
-    <a href="/js/">Logo</a>
-    <a href="/js/">CompIt</a>
-    <input type="text" placeholder=" Search..">
-    <a href="/js/">Favorite</a>
-    <a href="/js/">Login</a>
-</nav>
+
--- html/html_files/index.php
+++ html/html_files/index.php
@@ -1,25 +1,25 @@
 
 
-<body class="container">
-<div>
-    <h1>My First Heading</h1>
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-    <p>My first paragraph.</p>center
-</div>
-</body>
+    <body class="container" >
+        <div class="main"">
+            <h1>My First Heading</h1>
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+            <p>My first paragraph.</p>center
+        </div>
+    </body>
 </html>
 
