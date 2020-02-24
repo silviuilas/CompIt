2cc642a95b22d8a21b71e54a0ec3f694c088d98a
drgvszi <33577111+drgvszi@users.noreply.github.com>
2020-02-24 20:23:01 +0200

    Navbar Evolved

--- html/html_files/header.php
+++ html/html_files/header.php
@@ -8,14 +8,18 @@
     <link rel="stylesheet" href="../CSS/search.css">
     <link rel="icon" href="https://cdn4.iconfinder.com/data/icons/money-and-finance/512/find-512.png">
     <title>COMP IT</title>
+
     <nav class="navbar" >
-        <a href="/js/">Logo</a>
-        <a href="/js/">CompIt</a>
-        <input type="text" placeholder=" Search..">
-        <a href="/js/">Favorite</a>
-        <a href="/js/">Login</a>
+            <a href="#">Logo</a>
+            <a href="#">CompIt</a>
+            <input type="text" placeholder=" Search...">
+            <a href="#">Favorite</a>
+            <a href="#">Login</a>
+            <div> 
+                <button class="dropbtn">- 
+                </button>
+                
+            </div>
     </nav>
 </head>
-
-
 
--- html/html_files/index.php
+++ html/html_files/index.php
@@ -1,7 +1,7 @@
 
 
-    <body class="container" >
-        <div class="main"">
+   <body class="container" >
+        <div class="main">
             <h1>My First Heading</h1>
             <p>My first paragraph.</p>center
             <p>My first paragraph.</p>center
