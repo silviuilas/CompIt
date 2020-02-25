06f3783d73950abe94793b84272814fbcbb325e4
drgvszi <33577111+drgvszi@users.noreply.github.com>
2020-02-25 15:53:35 +0200

    Drop, login page(justuser)

--- html/html_files/header.php
+++ html/html_files/header.php
@@ -2,22 +2,26 @@
 
 
 <html lang="en">
-
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/search.css">
     <link rel="icon" href="https://cdn4.iconfinder.com/data/icons/money-and-finance/512/find-512.png">
     <title>COMP IT</title>
+
     <nav class="navbar" >
-            <img class="logo" src="https://cdn4.iconfinder.com/data/icons/money-and-finance/512/find-512.png">
-            <div class="moveup">
+            
+            <a href="../html/main.html">LG</a>
             <input type="text" placeholder="Search...">
-                <a href="#">Favorite</a>
-                <a href="#">Login</a>
-                <div class="dropdown">
-                    <button class="dropbtn"> --
-                    </button>
-                </div>
+            <a href="#">Favorite</a>
+            <div class="dropdown"> 
+                <button class="dropbtn"> --
+                </button>
+                <div class="dropcontent">
+                    <a href="#">Categorii</a>
+                    <a href="../html/login.html">Login</a>
+                    <a href="#">Sign Up</a>
+                    <a href="#">Your Opinion</a>
+                <div>
             </div>
     </nav>
 </head>
--- html/html_files/index.php
+++ html/html_files/index.php
@@ -1,7 +1,14 @@
 
 
    <body class="container" >
-          <div class="main">
+        <div class="oferte">
+                <ul>
+                    <li>Produs 1</li>
+                    <li>Produs 2</li>
+                    <li>Produs 3</li>
+                </ul>
+        </div>
+        <div class="main">
                 <h1>My First Heading</h1>
                 <p>My first paragraph.</p>center
                 <p>My first paragraph.</p>center
@@ -23,7 +30,6 @@
         <div class="footer">
                 <p>Dragos face ce trebe? </p>
         </div>
-
     </body>
 </html>
 
