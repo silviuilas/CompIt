a856008e96efe83ec3104b7a84c3ba389bac30fb
drgvszi <33577111+drgvszi@users.noreply.github.com>
2020-02-25 18:50:05 +0200

    Drop

--- html/html_files/header.php
+++ html/html_files/header.php
@@ -4,18 +4,19 @@
 <html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../CSS/search.css">
     <link rel="icon" href="https://cdn4.iconfinder.com/data/icons/money-and-finance/512/find-512.png">
     <title>COMP IT</title>
 
     <nav class="navbar" >
-            
-            <a href="../html/main.html">LG</a>
+            <a href="../html/main.html">CompIT</a>
             <input type="text" placeholder="Search...">
             <a href="#">Favorite</a>
             <div class="dropdown"> 
-                <button class="dropbtn"> --
-                </button>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+                </a>
                 <div class="dropcontent">
                     <a href="#">Categorii</a>
                     <a href="../html/login.html">Login</a>
@@ -25,4 +26,3 @@
             </div>
     </nav>
 </head>
-
