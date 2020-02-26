b4f6fbc24f273339e66fe41df2ca660fc0991fc3
silviu <silviuilas@gmail.com>
2020-02-26 12:23:37 +0200

    Fix Header Logo Link + PhpstormWarning

--- html/html_files/header.php
+++ html/html_files/header.php
@@ -8,21 +8,20 @@
     <link rel="stylesheet" href="../CSS/search.css">
     <link rel="icon" href="https://cdn4.iconfinder.com/data/icons/money-and-finance/512/find-512.png">
     <title>COMP IT</title>
-
-    <nav class="navbar" >
-            <a href="../html/index.php">CompIT</a>
-            <input type="text" placeholder="Search...">
-            <a href="#">Favorite</a>
-            <div class="dropdown"> 
-                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                <i class="fa fa-bars"></i>
-                </a>
-                <div class="dropcontent">
-                    <a href="#">Categorii</a>
-                    <a href="../html_files/login.php">Login</a>
-                    <a href="#">Sign Up</a>
-                    <a href="#">Your Opinion</a>
-                <div>
-            </div>
-    </nav>
 </head>
+<nav class="navbar" >
+    <a href="../">CompIT</a>
+    <input type="text" placeholder="Search...">
+    <a href="#">Favorite</a>
+    <div class="dropdown">
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+        <div class="dropcontent">
+            <a href="#">Categorii</a>
+            <a href="../html_files/login.php">Login</a>
+            <a href="#">Sign Up</a>
+            <a href="#">Your Opinion</a>
+            <div>
+            </div>
+</nav>
