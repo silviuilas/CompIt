557d95b34d18423b53beb5c10aa4734ee94614b0
silviu <silviuilas@gmail.com>
2020-02-28 03:29:47 +0200

    Added python Scraper For one page + Small example in PHP/pageGenerator.php

---
+++ html/Python/Scraper/scrapeOnePage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import bs4 as bs
+import urllib.request
+import json
+import sys
+
+def get_item(sauce,url):
+    soup = bs.BeautifulSoup(sauce,'lxml')
+    fullItem = {}
+    itemsArr = []
+    minprice=1000000
+    for div in soup.find_all('div',class_='optoffer device-desktop'):
+        oneItemObj = {
+            "shopname":div.find('div',class_="shopname").text,
+            "price"   :div.find('div',class_='row-price').text,
+            "link"    :div.find('a').get('href')
+        }
+        itemsArr.append(oneItemObj)
+        ronprice=oneItemObj['price'][:-7]
+        if (int(ronprice)< int(minprice)):
+            minprice = int(oneItemObj['price'][:-7])
+
+    fullItem ['name']     = soup.title.text[:-10]
+    fullItem ['url']      = url
+    fullItem ['minprice'] = str(minprice)
+    if(soup.find('div',class_="col-lg-3 col-md-3 col-sm-3 col-xs-4 product-image").find_all('a',href="True")):
+        fullItem ['imglink']  = soup.find('a',attrs={"class": "product-image-wrapper"}).get("href")
+    else:
+        fullItem ['imglink']  = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/google/80/black-question-mark-ornament_2753.png'
+    fullItem ['items']    = itemsArr
+    return fullItem
+
+url = sys.argv[1]
+sauce = urllib.request.urlopen(url).read()
+json_string = json.dumps(get_item(sauce,url))
+print(json_string)
