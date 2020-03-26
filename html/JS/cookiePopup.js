5055a3c7aee3529b088bbaff74a3783f4b3414f4
silviu <silviuilas@gmail.com>
2020-03-26 18:14:03 +0200

    Cookie popup + Applied OOP on items on the first page for easing extension later

---
+++ html/JS/cookiePopup.js
@@ -0,0 +1,26 @@
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+}
+function createCookie(name, value, days) {
+    var date, expires;
+    if (days) {
+        date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        expires = "; expires="+date.toGMTString();
+    } else {
+        expires = "";
+    }
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+if (!getCookie("cookie-popup")) {
+    $('.cookie-banner').delay(2000).fadeIn();
+}
+$("#cookie-accept-btn").click(function() {
+    createCookie("cookie-popup","accepted",30);
+    $('.cookie-banner').fadeOut();
+});
+
+
